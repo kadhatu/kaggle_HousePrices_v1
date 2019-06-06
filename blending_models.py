f3d145f05980763ca16b8c7c7e06e414b6ff37db
Xinchen <13816429999@163.com>
2019-06-06 11:09:02 +0800

    Update blending_models.py
    Prefer to running in Jupyter to check each code segment, and show up with image plotted.

--- blending_models.py
+++ blending_models.py
@@ -1,3 +1,6 @@
+# coding: utf-8
+
+# import modules
 import numpy as np
 import pandas as pd
 from datetime import datetime
@@ -33,15 +36,21 @@ warnings.filterwarnings('ignore')
 train = pd.read_csv("./train.csv")
 test = pd.read_csv("./test.csv")
 print("Data is loaded!")
+
+# print out to check
 print("Train:", train.shape[0],"sales, and", train.shape[1],"features")
 print("Test:", test.shape[0],"sales, and", test.shape[1],"features")
-
 
 # seperate quantitative and qualitative features
 quantitative = [f for f in train.columns if train.dtypes[f] != 'object']
 quantitative.remove('SalePrice')
 quantitative.remove('Id')
 qualitative = [f for f in train.columns if train.dtypes[f] == 'object']
+
+# print quantitative
+quantitative
+# print qualitative
+qualitative
 
 
 # plot missing numbers for each attribute
@@ -51,8 +60,7 @@ missing = missing[missing > 0]
 missing.sort_values(inplace=True)
 missing.plot.bar()
 
-
-# plot SalePrice distribution and try to log it.
+# plot SalePrice distribution and try to log it. See if it meets normal distribution
 y = train['SalePrice']
 plt.figure(1); plt.title('Johnson SU')
 sns.distplot(y, kde=False, fit=stats.johnsonsu)
@@ -62,14 +70,15 @@ plt.figure(3); plt.title('Log Normal')
 sns.distplot(y, kde=False, fit=stats.lognorm)
 
 
-# test if any quantitative variables has normal distribution.
+# test if any quantitative variables has normal distribution
 test_normality = lambda x: stats.shapiro(x.fillna(0))[1] < 0.01
 normal = pd.DataFrame(train[quantitative])
 normal = normal.apply(test_normality)
 print(not normal.any())
 
 
-# Spearman correlation
+# prepare to plot spearman correlation image
+# define encode function
 def encode(frame, feature):
     ordering = pd.DataFrame()
     ordering['val'] = frame[feature].unique()
@@ -81,7 +90,7 @@ def encode(frame, feature):
     for cat, o in ordering.items():
         frame.loc[frame[feature] == cat, feature + '_E'] = o
 
-# Turn qualitative features into numberic
+# use encode function to Turn qualitative features into numberic     
 qual_encoded = []
 for q in qualitative:
     encode(train, q)
@@ -102,6 +111,10 @@ def spearman(frame, features):
 features = quantitative + qual_encoded
 
 
+# print features to check
+features
+
+
 # plot the correlation heatmap for both quantitative and qualitative encoded
 plt.figure(1)
 corr = train[quantitative+['SalePrice']].corr()
@@ -110,7 +123,8 @@ plt.figure(2)
 corr = train[qual_encoded+['SalePrice']].corr()
 sns.heatmap(corr)
 plt.figure(3)
-corr = pd.DataFrame(np.zeros([len(quantitative)+1, len(qual_encoded)+1]), index=quantitative+['SalePrice'], columns=qual_encoded+['SalePrice'])
+corr = pd.DataFrame(np.zeros([len(quantitative)+1, len(qual_encoded)+1]), 
+                index=quantitative+['SalePrice'], columns=qual_encoded+['SalePrice'])
 for q1 in quantitative + ['SalePrice']:
     for q2 in qual_encoded + ['SalePrice']:
         corr.loc[q1, q2] = train[q1].corr(train[q2])
@@ -123,6 +137,7 @@ model = TSNE(n_components=2, random_state=0, perplexity=50)
 X = train[features].fillna(0.).values
 tsne = model.fit_transform(X)
 
+# using PCA
 std = StandardScaler()
 s = std.fit_transform(X)
 pca = PCA(n_components=30)
@@ -137,19 +152,17 @@ sns.lmplot(data=fr, x='tsne1', y='tsne2', hue='cluster', fit_reg=False)
 print(np.sum(pca.explained_variance_ratio_))
 
 
-
-# Models
+# Preparing for models
 # Data processing
-
 train.drop(['Id'], axis=1, inplace=True)
 test.drop(['Id'], axis=1, inplace=True)
+
 train = train[train.GrLivArea < 4500]
 train.reset_index(drop=True, inplace=True)
 train["SalePrice"] = np.log1p(train["SalePrice"])
 y = train['SalePrice'].reset_index(drop=True)
 
-
-# Feature
+# Feature processing
 train_features = train.drop(['SalePrice'], axis=1)
 test_features = test
 features = pd.concat([train_features, test_features]).reset_index(drop=True)
@@ -166,14 +179,18 @@ features['Exterior1st'] = features['Exterior1st'].fillna(features['Exterior1st']
 features['Exterior2nd'] = features['Exterior2nd'].fillna(features['Exterior1st'].mode()[0])
 features['SaleType'] = features['SaleType'].fillna(features['SaleType'].mode()[0])
 
-# fill N/A 
+# print features' head 5 lines to check
+features.head()
+
+
+# fill N/A
 for col in ('GarageYrBlt', 'GarageArea','GarageCars'):
     features[col] = features[col].fillna(0)
 for col in ['GarageType', 'GarageFinish', 'GarageQual', 'GarageCond']:
     features[col] = features[col].fillna('None')
 for col in ['BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2']:
     features[col] = features[col].fillna('None')
-    
+
 features['MSZoning'] = features.groupby('MSSubClass')['MSZoning'].transform(lambda x: x.fillna(x.mode()[0]))
 
 
@@ -187,7 +204,6 @@ features.update(features[objects].fillna('None'))
 # fill N/A with median
 features['LotFrontage'] = features.groupby('Neighborhood')['LotFrontage'].transform(lambda x: x.fillna(x.median()))
 
-
 # fill N/A in numeric dtypes features with zero
 numeric_dtypes = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
 numerics = []
@@ -195,7 +211,6 @@ for i in features.columns:
     if features[i].dtype in numeric_dtypes:
         numerics.append(i)
 features.update(features[numerics].fillna(0))
-
 
 numeric_dtypes = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
 numerics2 = []
@@ -209,7 +224,7 @@ skew_index = high_skew.index
 
 for i in skew_index:
     features[i] = boxcox1p(features[i], boxcox_normmax(features[i] + 1))
-    
+
 
 # Drop features
 features = features.drop(['Utilities', 'Street', 'PoolQC'], axis=1)
@@ -217,18 +232,30 @@ features = features.drop(['Utilities', 'Street', 'PoolQC'], axis=1)
 # Merge features
 features['YrBltAndRemod'] = features['YearBuilt'] + features['YearRemodAdd']
 features['TotalSF'] = features['TotalBsmtSF'] + features['1stFlrSF'] + features['2ndFlrSF']
-features['Total_sqr_footage'] = (features['BsmtFinSF1'] + features['BsmtFinSF2'] + features['1stFlrSF'] + features['2ndFlrSF'])
-features['Total_Bathrooms'] = (features['FullBath'] + (0.5 * features['HalfBath']) + features['BsmtFullBath'] + (0.5 * features['BsmtHalfBath']))
-features['Total_porch_sf'] = (features['OpenPorchSF'] + features['3SsnPorch'] + features['EnclosedPorch'] + features['ScreenPorch'] + features['WoodDeckSF'])
+features['Total_sqr_footage'] = (features['BsmtFinSF1'] + features['BsmtFinSF2'] + \
+                                 features['1stFlrSF'] + features['2ndFlrSF'])
+features['Total_Bathrooms'] = (features['FullBath'] + (0.5 * features['HalfBath']) + \
+                                 features['BsmtFullBath'] + (0.5 * features['BsmtHalfBath']))
+features['Total_porch_sf'] = (features['OpenPorchSF'] + features['3SsnPorch'] + \
+                features['EnclosedPorch'] + features['ScreenPorch'] + features['WoodDeckSF'])
+
 features['haspool'] = features['PoolArea'].apply(lambda x: 1 if x > 0 else 0)
 features['has2ndfloor'] = features['2ndFlrSF'].apply(lambda x: 1 if x > 0 else 0)
 features['hasgarage'] = features['GarageArea'].apply(lambda x: 1 if x > 0 else 0)
 features['hasbsmt'] = features['TotalBsmtSF'].apply(lambda x: 1 if x > 0 else 0)
 features['hasfireplace'] = features['Fireplaces'].apply(lambda x: 1 if x > 0 else 0)
 
+
+# check features' shape
+features.shape
+
 # set dummies
 final_features = pd.get_dummies(features).reset_index(drop=True)
 
+# check features' shape
+final_features.shape
+
+# handling on features
 X = final_features.iloc[:len(y), :]
 X_sub = final_features.iloc[len(y):, :]
 X.shape, y.shape, X_sub.shape
@@ -239,20 +266,23 @@ outliers = [30, 88, 462, 631, 1322]
 X = X.drop(X.index[outliers])
 y = y.drop(y.index[outliers])
 
-# drop overfit
 overfit = []
 for i in X.columns:
     counts = X[i].value_counts()
     zeros = counts.iloc[0]
     if zeros / len(X) * 100 > 99.94:
         overfit.append(i)
-        
+
 overfit = list(overfit)
 X = X.drop(overfit, axis=1)
 X_sub = X_sub.drop(overfit, axis=1)
 
 
-# 
+# print & check
+X.shape, y.shape, X_sub.shape
+
+
+# define KFold and RMSE
 kfolds = KFold(n_splits=10, shuffle=True, random_state=42)
 def rmsle(y, y_pred):
     return np.sqrt(mean_squared_error(y, y_pred))
@@ -261,18 +291,20 @@ def cv_rmse(model, X=X):
     rmse = np.sqrt(-cross_val_score(model, X, y, scoring='neg_mean_squared_error', cv=kfolds))
     return rmse
 
-
-# 
+# define hyper param
 alphas_alt = [14.5, 14.6, 14.7, 14.8, 14.9, 15, 15.1, 15.2, 15.3, 15.4, 15.5]
 alphas2 = [5e-05, 0.0001, 0.0002, 0.0003, 0.0004, 0.0005, 0.0006, 0.0007]
 e_alphas = [0.0001, 0.0002, 0.0003, 0.0004, 0.0005, 0.0006, 0.0007]
 e_l1ratio = [0.8, 0.85, 0.9, 0.95, 0.99, 1]
 
+# generate models
 ridge = make_pipeline(RobustScaler(), RidgeCV(alphas=alphas_alt, cv=kfolds))
 lasso = make_pipeline(RobustScaler(), LassoCV(max_iter=1e7, alphas=alphas2, random_state=42, cv=kfolds))
-elasticnet = make_pipeline(RobustScaler(), ElasticNetCV(max_iter=1e7, alphas=e_alphas, cv=kfolds, l1_ratio=e_l1ratio))
+elasticnet = make_pipeline(RobustScaler(), ElasticNetCV(max_iter=1e7, alphas=e_alphas, 
+                                                        cv=kfolds, l1_ratio=e_l1ratio))
 svr = make_pipeline(RobustScaler(), SVR(C=20, epsilon=0.008, gamma=0.0003))
-gbr = GradientBoostingRegressor(n_estimators=300, learning_rate=0.05, max_depth=4, max_features='sqrt', min_samples_leaf=15, min_samples_split=10, loss='huber', random_state=42)
+gbr = GradientBoostingRegressor(n_estimators=300, learning_rate=0.05, max_depth=4, max_features='sqrt', 
+                                min_samples_leaf=15, min_samples_split=10, loss='huber', random_state=42)
 lightgbm = LGBMRegressor(objective='regression', num_leaves=4,
                         learning_rate=0.01, n_estimators=5000,
                         max_bin=200, bagging_fraction=0.75,
@@ -284,12 +316,13 @@ xgboost = XGBRegressor(learning_rate=0.01, n_estimators=3460, max_depth=3,
                       colsample_bytree=0.7, objective='reg:linear',
                       nthread=-1, scale_pos_weight=1, seed=27,
                       reg_alpha=0.00006)
+
 stack_gen = StackingCVRegressor(regressors=(ridge, lasso, elasticnet, gbr, xgboost, lightgbm),
                                meta_regressor=xgboost,
                                use_features_in_secondary=True)
 
 
-# 
+# print out scores' mean and std for each model
 score = cv_rmse(ridge)
 score = cv_rmse(lasso)
 print("LASSO: {:.4f} ({:.4f})\n".format(score.mean(), score.std()), datetime.now(), )
@@ -310,26 +343,31 @@ score = cv_rmse(xgboost)
 print("xgboost: {:.4f} ({:.4f})\n".format(score.mean(), score.std()), datetime.now(), )
 
 
-
-# 
+# Began to fit
 print('START Fit')
 print('stack_gen')
 stack_gen_model = stack_gen.fit(np.array(X), np.array(y))
+
 print('elasticnet')
 elastic_model_full_data = elasticnet.fit(X, y)
+
 print('Lasso')
 lasso_model_full_data = lasso.fit(X, y)
+
 print('Ridge')
 ridge_model_full_data = ridge.fit(X, y)
+
 print('Svr')
 svr_model_full_data = svr.fit(X, y)
+
 print('GradientBoosting')
 gbr_model_full_data = gbr.fit(X, y)
+
 print('xgboost')
 xgb_model_full_data = xgboost.fit(X, y)
+
 print('lightgbm')
 lgb_model_full_data = lightgbm.fit(X, y)
-
 
 
 # Blending Models
@@ -352,11 +390,6 @@ submission = pd.read_csv("./sample_submission.csv")
 submission.iloc[:,1] = np.floor(np.expm1(blend_models_predict(X_sub)))
 
 
-# Generate submission
+# Generate submission to submission_v1.csv
 submission['SalePrice'] = np.floor(np.expm1(blend_models_predict(X_sub)))
-submission.to_csv('test_submission.csv', index=False)
-
-
-
-
-
+submission.to_csv('submission_v1.csv', index=False)
